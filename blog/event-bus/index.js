91cfde8f26c8723770d5c114840764484c070018
Sebastian Miranda <arqcompu4@gmail.com>
2021-08-10 19:52:18 -0500

    docker files and first config file kubernets

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -17,10 +17,10 @@ app.post('/events', (req, res) => {
 
     events.push(event);
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-    axios.post('http://localhost:4003/events', event);
+    axios.post('http://localhost:4000/events', event); // posts service
+    axios.post('http://localhost:4001/events', event); // comments service
+    axios.post('http://localhost:4002/events', event); // query service 
+    axios.post('http://localhost:4003/events', event); // moderation service 
 
     res.send({status: 'OK'});
 });
